15cb8d555b1ef1d3ff1fb0d9aa216e0f03e092a0
Detroiman <52424452+Detroiman@users.noreply.github.com>
2021-07-10 01:51:53 +0300

    First
    rework the reading Json file

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,20 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 import 'dart:convert';
 
-void main() {
-  runApp(MyApp());
+
+Future<List> getJsonFile() async {
+  String url = 'https://raw.githubusercontent.com/ababicheva/FlutterInternshipTestTask/main/recipes.json';
+  http.Response response = await http.get(url);
+  return json.decode(response.body);
 }
 
-class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: MainPage(
-      ),
-    );
-  }
-
-}
-
-class MainPage extends StatefulWidget{
-  @override
-  _MainPage createState() => _MainPage();
-}
-
-class _MainPage extends State<MainPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+void main() async {
+  List _dataFromGit = await getJsonFile();
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new Scaffold(
       appBar: AppBar(
         title: const Text('My Recipes'),
         actions: [
@@ -37,49 +25,71 @@ class _MainPage extends State<MainPage> {
           ),
         ],
       ),
-      body: Center(
-        child: new FutureBuilder(builder: (context, snapshot){
-          var myData = json.decode(snapshot.data.toString());
-          return new ListView.builder(
-              itemCount: myData.length,
-              itemBuilder: (BuildContext context, int index){
-                return Column(
-                  children: [
-
-                    ListTile(
-                  title: new Text(
-                    'Name' + myData[index]['name'],
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  subtitle: new Text('${myData[index]['description']}',
-                    style: TextStyle(fontSize: 17),),
-                  minLeadingWidth: 50,
-                    leading: ConstrainedBox(
-                      constraints: BoxConstraints(
-                        maxWidth: 86,
-                        maxHeight: 76,
-                        minHeight: 56,
-                        minWidth: 66,
-                      ),
-                      child: Image.network(myData[index]['picture'],
-                          fit: BoxFit.cover, width: 100, height: 150),
-                    ),
-                  trailing: Text(
-                      '${index+1}',
-                  ),
+      body: ListView.builder(
+        itemCount: _dataFromGit.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
+            children: [
+            ListTile(
+            contentPadding:
+            EdgeInsets.symmetric(vertical: 10.0, horizontal: 7.0),
+            title: Text(
+              "${_dataFromGit[index]['name'][0].toString().toUpperCase()}${_dataFromGit[index]['name'].toString().substring(1)}",
+              style: TextStyle(
+                fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+              ),
+            ),
+            leading: new ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: 86,
+                maxHeight: 76,
+                minHeight: 56,
+                minWidth: 66,
+              ),
+              child: Image.network('${_dataFromGit[index]['picture']}', height: 150, width: 200, fit: BoxFit.cover,),
+            ),
+              trailing: Text(
+                '${index+1}',
+                style: TextStyle(
+                  fontSize: 20,
                 ),
-                  Divider(
-                    thickness: 2,
-                    color: Colors.grey.shade300,
-                    indent: 105,
-                  ),
-                  ]
-                );
-              },
-              );
-        },future: DefaultAssetBundle.of(context)
-            .loadString('assetses/recipes.json'),),
+              ),
+
+            onTap: () {
+              _getInfoAboutProduct(context, "${_dataFromGit[index]['description'][0].toString().toUpperCase()}${_dataFromGit[index]['description'].toString().substring(1)}",
+                  "${_dataFromGit[index]['name'][0].toString().toUpperCase()}${_dataFromGit[index]['name'].toString().substring(1)}");
+            },
+            subtitle: Text(
+              "${_dataFromGit[index]['description'][0].toString().toUpperCase()}${_dataFromGit[index]['description'].toString().substring(1)}",
+              style: TextStyle(fontSize: 15.0),
+            ),
+          ),
+              Divider(
+                thickness: 2,
+                color: Colors.grey.shade300,
+                indent: 105,
+              ),]
+          );
+        },
       ),
-    );
-  }
+    ),
+  ));
 }
+
+void _getInfoAboutProduct(BuildContext data, String tex, String titles) {
+  var dialogWindow = new AlertDialog(
+    title: new Text(titles),
+    content: new Text(tex),
+    actions: <Widget>[
+      new TextButton(
+        child: new Text("Good"),
+        onPressed: () {
+          Navigator.pop(data);
+        },
+      )
+    ],
+  );
+  showDialog(context: data, builder: (context) => dialogWindow);
+}
+
